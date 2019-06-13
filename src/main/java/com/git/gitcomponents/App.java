db04cfb766d4459044b4eb8dcc15c8d696fa9a36
karthikkone <kirti.annajigar@ad.infosys.com>
2019-06-13 15:58:28 +0530

    Update App.java

--- src/main/java/com/git/gitcomponents/App.java
+++ src/main/java/com/git/gitcomponents/App.java
@@ -2,6 +2,7 @@ package com.git.gitcomponents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@EntityScan("com.git.gitcomponents.NetClientGet")
 public class App 
 {
     public static void main( String[] args )
