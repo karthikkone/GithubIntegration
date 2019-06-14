0101b8b3b8a98937f1c811feb7cc504c4969b00c
karthikkone <kirti.annajigar@ad.infosys.com>
2019-06-14 15:27:36 +0530

    Update NetClientGet.java

--- src/main/java/com/git/gitcomponents/NetClientGet.java
+++ src/main/java/com/git/gitcomponents/NetClientGet.java
@@ -13,44 +13,22 @@ import com.google.gson.ExclusionStrategy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import net.sf.json.JSONObject;
 import okhttp3.*;
 @RestController
 public class NetClientGet {
 	private DataRepository dataRepository;
-	// http://localhost:8080/RESTfulExample/json/product/get
-@RequestMapping(value="/getComponents", method=RequestMethod.GET)
-public void getComponents() {
+	
+@RequestMapping(value="/getComponents", params={"accountname","reponame"},method=RequestMethod.GET)
+public void getComponents(String accountname,String reponame) {
 
-  try {
-
-	/*URL url = new URL("https://api.github.com/repos/karthikkone/DemoRepo/git/trees/686b02625baf89bd828480d0a3bcbb25fb2596d9");
-	HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-	conn.setRequestMethod("GET");
-	conn.setRequestProperty("Accept", "application/json");
-
-	if (conn.getResponseCode() != 200) {
-		throw new RuntimeException("Failed : HTTP error code : "
-				+ conn.getResponseCode());
-	}
-
-BufferedReader br = new BufferedReader(new InputStreamReader(
-	(conn.getInputStream())));
-
-String output;
-System.out.println("Output from Server .... \n");
-while ((output = br.readLine()) != null) {
-	System.out.println(output);
-}	
-		conn.disconnect();*/
+  try {	
 	  
 	  OkHttpClient httpClient = new OkHttpClient();
-	  /*HttpUrl ghURL = HttpUrl.parse("https://api.github.com/repos/karthikkone/DemoRepo/git/trees/686b02625baf89bd828480d0a3bcbb25fb2596d9").newBuilder()
-              .addPathSegment("")
-              .build();*/
-
+	  String url = "https://api.github.com/repos/"+accountname+"/"+reponame+"/"+"contents";
 	  Request request = new Request.Builder()
       		.header("Accept", "application/json")
-              .url("https://api.github.com/repos/karthikkone/DemoRepo/contents")
+              .url(url)
               .get()
               .build();
 	  
@@ -62,11 +40,15 @@ while ((output = br.readLine()) != null) {
 	  Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
       
       RepoData[] repoData = gson.fromJson(resp, RepoData[].class);
+      //JSONObject json = new JSONObject();
       for(int i=0; i< repoData.length ;i++)
       {
       System.out.println("gson repoData :"+repoData[i]);
-      //dataRepository.saveAndFlush(repoData);
-      //System.out.println("repoData :"+dataRepository.findAll());
+      if(repoData[i].getPath() == "src")
+      {
+    	  //call another method sendinf src sha ..
+    	  getMetadataType(accountname,reponame,repoData[i].getSha());
+      }
       }
 	  }
 
@@ -77,5 +59,41 @@ while ((output = br.readLine()) != null) {
 	  }
 
 	}
+	public void getMetadataType(String accountname,String reponame,String sha)
+	{
+		try {	
+			  
+			  OkHttpClient httpClient = new OkHttpClient();
+			  String url = "https://api.github.com/repos/"+accountname+"/"+reponame+"/"+"/git/trees/"+sha;
+			  Request request = new Request.Builder()
+		      		.header("Accept", "application/json")
+		              .url(url)
+		              .get()
+		              .build();
+			  
+			  Response response = httpClient.newCall(request).execute();
+			  if(response.code() == 200)
+			  {
+			  String resp = response.body().string();
+			  System.out.println("output :"+resp);
+			  /*Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+		      
+		      RepoData[] repoData = gson.fromJson(resp, RepoData[].class);
+		      //JSONObject json = new JSONObject();
+		      for(int i=0; i< repoData.length ;i++)
+		      {
+		      System.out.println("gson repoData :"+repoData[i]);
+		      if(repoData[i].getPath() == "src")
+		      {
+		    	  //call another method sendinf src sha ..
+		      }
+		      }*/
+			  }
 
+			  } catch (Exception e) {
+
+				e.printStackTrace();
+
+			  }
+	}
 }
