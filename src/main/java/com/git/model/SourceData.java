75eb3d13900b8e53c462197debecb440f8b99e8d
karthikkone <kirti.annajigar@ad.infosys.com>
2019-06-14 16:24:13 +0530

    Update SourceData.java

--- src/main/java/com/git/model/SourceData.java
+++ src/main/java/com/git/model/SourceData.java
@@ -9,4 +9,44 @@ public class SourceData {
 	@Expose @SerializedName("tree")
 	SourceType sourceType;
 
+	public SourceType getSourceType() {
+		return sourceType;
+	}
+
+	public void setSourceType(SourceType sourceType) {
+		this.sourceType = sourceType;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((sourceType == null) ? 0 : sourceType.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SourceData other = (SourceData) obj;
+		if (sourceType == null) {
+			if (other.sourceType != null)
+				return false;
+		} else if (!sourceType.equals(other.sourceType))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "SourceData [sourceType=" + sourceType + "]";
+	}
+	
+	
+
 }
