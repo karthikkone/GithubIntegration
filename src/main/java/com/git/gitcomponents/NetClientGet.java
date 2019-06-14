8c76c18d19f9f2ef844f2078bcf6d2365cb94e44
karthikkone <kirti.annajigar@ad.infosys.com>
2019-06-14 11:31:50 +0530

    Update NetClientGet.java

--- src/main/java/com/git/gitcomponents/NetClientGet.java
+++ src/main/java/com/git/gitcomponents/NetClientGet.java
@@ -62,6 +62,7 @@ while ((output = br.readLine()) != null) {
 	  Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
       
       RepoData repoData = gson.fromJson(resp, RepoData.class);
+      System.out.println("repoData :"+repoData);
 	  }
 
 	  } catch (Exception e) {
